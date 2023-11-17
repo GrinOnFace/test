44391d68b849c32fce8270e3ddd66457e6e0d8b2
GrinOnFace <109618694+GrinOnFace@users.noreply.github.com>
2023-11-17 22:32:58 +0300

    test

---
+++ go.js
@@ -0,0 +1,23 @@
+var majorityElement = function(nums) {
+    let obj = {}
+    let array = []
+    for (let i = 0; i < nums.length; i++){
+        let elem = nums[i]
+        if (obj[elem] in obj){
+            obj[elem]++
+        }
+        else{   
+            obj[elem] = 1
+        }
+    }
+    console.log(obj)
+    for (let i = 0; i < Object.values(obj).length; i++){
+        console.log(Object.values(obj)[i])
+        if (Object.values(obj)[i] > nums.length/3){
+            array.push(Object.keys(obj)[i])
+        }
+    }
+    return array
+};
+
+console.log(majorityElement([2,3,3]))
